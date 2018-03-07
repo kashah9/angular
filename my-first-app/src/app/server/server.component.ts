08a7a2433ba89f081b3649f8b7542b03f0e10b5b
kashah9 <skindia20@gmail.com>
2018-03-07 00:14:08 -0500

    event and property binding
    Overview of event and property binding

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
 
     // String interpolation 
     serverID: number = 10;
@@ -12,5 +12,19 @@ export class ServerComponent {
 
     getSeverStatus(){
         return this.serverStatus;
+    }
+
+    allowNewServer = false;
+    serverCreation = "Server not created!";
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000);  
+    }
+    ngOnInit() {
+    } 
+
+    onCreateServer(){
+        this.serverCreation = "server created, yay!"
     }
 }
