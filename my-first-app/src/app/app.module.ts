c79c95b78e557d544d05b37960222b846c8e11f7
Karan Shah <skindia20@gmail.com>
2018-03-20 22:21:31 -0400

    Transition
    transition to new environment

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'; // Necessary for two-way binding
+import { FormsModule } from '@angular/forms'; // Necessary for two-way binding
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
